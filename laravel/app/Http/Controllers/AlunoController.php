bba71ee24c752f188f959fc97d7977e96e4b4610
LarissaSudo17 <sudolarissa@gmail.com>
2026-08-19 12:20:49 -0300

    commit 19/08

--- laravel/app/Http/Controllers/AlunoController.php
+++ laravel/app/Http/Controllers/AlunoController.php
@@ -2,21 +2,70 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreAlunoRequest;
+use App\Http\Requests\UpdateAlunoRequest;
+use App\Models\Aluno;
 
 class AlunoController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $alunos = [
+            ["nome"=>"Miranata", "email"=>"m@hotmail.com"],
+            ["nome"=>"Barata", "email"=>"b@hotmail.com"],
+        ];
 
-    function listar(){ 
-         return response()->json([
-            ["nome"=> "Amaureca","rm"=>1,"email"=>"ocarecamaismaisdeTatui@gmail.com"], 
-            ["nome"=> "Chimabrunão","rm"=>2,"email"=>"opaulistamaisgaucho@gmail.com"],
-             ["nome"=> "Diogo","rm"=>4,"email"=>"ojacksonmaisbranco@gmail.com"],
-        ]
-            );
+        return $alunos;
     }
-    function adicionar(){ }
-    function remove(){ }
-    function update(){ }
 
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAlunoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Aluno $aluno)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Aluno $aluno)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAlunoRequest $request, Aluno $aluno)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Aluno $aluno)
+    {
+        //
+    }
 }
