819726f83e3a4a60344df213a02961d554bb42a7
LarissaSudo17 <sudolarissa@gmail.com>
2026-09-02 11:42:41 -0300

    commit 4 02/09

--- laravel/database/seeders/ContatoModelSeeder.php
+++ laravel/database/seeders/ContatoModelSeeder.php
@@ -2,16 +2,21 @@
 
 namespace Database\Seeders;
 
+use App\Models\ContatoModel;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class ContatoSeeder extends Seeder
+
+class ContatoModelSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+       $contato= new ContatoModel();
+       $contato->nome = "Larissa";
+       $contato->email = "sudolarissa@gmail.com";
+       $contato->save();
     }
 }
